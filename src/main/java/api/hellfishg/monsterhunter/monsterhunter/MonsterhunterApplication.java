283b2b0b1f07c3d65163fa200512045e69019c5c
hellfishg <hellfish.g@protonmail.com>
2025-03-18 19:11:06 +0100

    monsterHunter INIT

---
+++ src/main/java/api/hellfishg/monsterhunter/monsterhunter/MonsterhunterApplication.java
@@ -0,0 +1,13 @@
+package api.hellfishg.monsterhunter.monsterhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterhunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterhunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/hellfishg/monsterhunter/monsterhunter/MonsterhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package api.hellfishg.monsterhunter.monsterhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

19276b066297ebbd84d10e37e32219ec91a77f10
hellfishg <hellfish.g@protonmail.com>
2025-03-21 18:23:45 +0100

    - Test unitarios para landinPageController.getLandingPage() - Refactorizacion del POM por dependencias duplicadas Junit y mockito.

--- src/main/java/api/hellfishg/monsterhunter/monsterhunter/controller/LandingPageController.java
+++ src/main/java/api/hellfishg/monsterhunter/monsterhunter/controller/LandingPageController.java
@@ -1,26 +1,25 @@
 package api.hellfishg.monsterhunter.monsterhunter.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import api.hellfishg.monsterhunter.monsterhunter.model.enums.MenusEnum;
 
-@Controller
+@RestController
 public class LandingPageController {
 
     @GetMapping("/")
-    public String getOptionsList(@RequestParam String param) {
-        Map<Integer, MenusEnum.MENUS> optionsList = new HashMap<>();
-        optionsList.put(1, MenusEnum.MENUS.BIOMAS );
-        optionsList.put(2, MenusEnum.MENUS.MONSTRUOS );
-        optionsList.put(3, MenusEnum.MENUS.ARMAS );
-        optionsList.put(4, MenusEnum.MENUS.ARMADURAS );
-        optionsList.put(4, MenusEnum.MENUS.ITEMS );
-        return "Rady to go";
+     public ResponseEntity<List<MenusEnum.MENUS>> getOptionsList() {
+        List<MenusEnum.MENUS> optionsList = new ArrayList<>();
+        optionsList.add(MenusEnum.MENUS.BIOMAS);
+        optionsList.add(MenusEnum.MENUS.MONSTRUOS);
+        optionsList.add(MenusEnum.MENUS.ARMAS);
+        optionsList.add(MenusEnum.MENUS.ARMADURAS);
+        optionsList.add(MenusEnum.MENUS.ITEMS);
+        return ResponseEntity.ok(optionsList);
     }
-
 }
--- src/test/java/api/hellfishg/monsterhunter/monsterhunter/controller/LandingPageControllerTest.java
+++ src/test/java/api/hellfishg/monsterhunter/monsterhunter/controller/LandingPageControllerTest.java
@@ -1,16 +1,27 @@
 package api.hellfishg.monsterhunter.monsterhunter.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+@WebMvcTest(LandingPageController.class)
 public class LandingPageControllerTest {
 
-    @Test
-    void getLandingPage() {
-        LandingPageController LandingPage = new LandingPageController();
-
-        assertEquals(LandingPage, LandingPage);
-    }
+    @Autowired
+    private MockMvc mockMvc;
     
+    LandingPageController landingPageController = new LandingPageController();
+
+    @Test
+    public void getLandingPage() throws Exception{
+        String respuestaMenuItemsJson = "[\"BIOMAS\", \"MONSTRUOS\", \"ARMAS\", \"ARMADURAS\", \"ITEMS\"]";
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(respuestaMenuItemsJson));
+    }
 }
